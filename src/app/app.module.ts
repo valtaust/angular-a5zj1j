bb8190a55f210014fe1f754c23aaa28f9013640d
valtaust <valtaust93@gmail.com>
2022-07-12 18:32:19 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,13 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },//added this in after generating page
