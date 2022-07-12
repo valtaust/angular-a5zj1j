e9c5ddbaaf2174b5f5b7c0de285bb1f45124d727
valtaust <valtaust93@gmail.com>
2022-07-12 18:20:00 +0100

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,5 +6,22 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
+
   constructor() { }
+
+  
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+
 }
